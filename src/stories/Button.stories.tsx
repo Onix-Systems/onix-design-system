4cacca59d4f21232aa1b626e72946117a8b1abcc
IvanHoncharenko <ivan.honcharenko@onix-systems.com>
2023-04-10 12:25:11 +0300

    Updated dependencies

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Button from '../components/Button/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Default',
+};
