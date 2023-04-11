34821b067e43c169983128ca5bcfb231944264bb
IvanHoncharenko <ivan.honcharenko@onix-systems.com>
2023-04-11 11:47:31 +0300

    Basic bell button created

---
+++ src/components/Bell/Bell.tsx
@@ -0,0 +1,20 @@
+import React, { FC, memo } from 'react';
+import { IBellProps } from './interfaces/IBell';
+import styles from './sass/Bell.module.scss';
+
+import BellIcon from '../../assets/icons/bell.svg';
+
+const Bell: FC<IBellProps> = ({
+  onClick,
+  alt = 'bell',
+  isActive = false,
+  hasNotification = false,
+  customClass = '',
+}) => (
+  <button className={`${styles.bellButton} ${customClass}`} type="button" onClick={onClick}>
+    <img className={isActive ? `${styles.activeBell}` : ''} src={BellIcon} alt={alt} />
+    {hasNotification && <div className={styles.notificationDot} />}
+  </button>
+);
+
+export default memo(Bell);
---
+++ src/components/Bell/interfaces/IBell.ts
@@ -0,0 +1,7 @@
+export interface IBellProps {
+  onClick: () => void;
+  alt?: string;
+  isActive?: boolean;
+  hasNotification?: boolean;
+  customClass?: string;
+}
