b2125a2391d0dd5033f93fe34c41e35592dcc308
IvanHoncharenko <ivan.honcharenko@onix-systems.com>
2023-04-13 12:18:01 +0300

    REACT-326-1 Icon rollup export implemented

--- src/components/icons.ts
+++
@@ -1,5 +0,0 @@
-export { default as CloseIcon } from '../assets/icons/close.svg';
-export { default as CloseRedIcon } from '../assets/icons/closeRed.svg';
-export { default as CloseRedBigIcon } from '../assets/icons/redBigClose.svg';
-export { default as CheckmarkGreenIcon } from '../assets/icons/greenCheckmark.svg';
-export { default as RoundArrowsIcon } from '../assets/icons/roundArrows.svg';
