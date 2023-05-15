a4efb4b27acc5dedd10c05ac63dd10a810754be7
IvanHoncharenko <ivan.honcharenko@onix-systems.com>
2023-05-15 11:45:32 +0300

    Added typography to dropdown

--- src/components/Dropdown/components/DropdownItem.tsx
+++ src/components/Dropdown/components/DropdownItem.tsx
@@ -1,11 +1,11 @@
 import React, { FC } from 'react';
 import { IDropdownItem } from '../interfaces/IDropdown';
+import { TextBig } from '../../Typography/Typography';
 import styles from '../sass/DropdownItem.module.scss';
 
 const DropdownItem: FC<IDropdownItem> = ({ customClass, name, onClick }) => (
-  // TODO: Change to typography from another branch
   <div onClick={onClick} role="presentation" className={`${styles.container} ${customClass}`}>
-    <p>{name}</p>
+    <TextBig>{name}</TextBig>
   </div>
 );
 
