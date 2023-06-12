b9df2f6b1b4f4362d51dd90ad54a85f824794731
IvanHoncharenko <ivan.honcharenko@onix-systems.com>
2023-06-12 16:58:33 +0300

    new-dropdown InputFilterDropdown implemented

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,37 +1,38 @@
 import React, { FC } from 'react';
 import styles from './sass/Typography.module.scss';
+import { ITypographyProps } from './interfaces/ITypography';
 
-export const H1: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const H1: FC<ITypographyProps> = ({ children }) => (
   <h1 className={styles.h1}>{children}</h1>
 );
 
-export const TextLarge: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextLarge: FC<ITypographyProps> = ({ children }) => (
   <p className={styles.textLarge}>{children}</p>
 );
 
-export const TextLargeBold: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextLargeBold: FC<ITypographyProps> = ({ children }) => (
   <p className={`${styles.textLarge} ${styles.bold}`}>{children}</p>
 );
 
-export const TextBig: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextBig: FC<ITypographyProps> = ({ children }) => (
   <p className={styles.textBig}>{children}</p>
 );
 
-export const TextBigBold: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextBigBold: FC<ITypographyProps> = ({ children }) => (
   <p className={`${styles.textBig} ${styles.bold}`}>{children}</p>
 );
 
-export const TextMedium: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextMedium: FC<ITypographyProps> = ({ children }) => (
   <p className={styles.textMedium}>{children}</p>
 );
 
-export const TextMediumBold: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextMediumBold: FC<ITypographyProps> = ({ children }) => (
   <p className={`${styles.textMedium} ${styles.bold}`}>{children}</p>
 );
-export const TextSmall: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextSmall: FC<ITypographyProps> = ({ children }) => (
   <p className={styles.textSmall}>{children}</p>
 );
 
-export const TextExtraSmall: FC<{ children: React.ReactNode }> = ({ children }) => (
+export const TextExtraSmall: FC<ITypographyProps> = ({ children }) => (
   <p className={styles.textExtraSmall}>{children}</p>
 );
